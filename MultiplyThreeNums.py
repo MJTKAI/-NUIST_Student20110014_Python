1a6e9b6aae1107b9757c64e7a0f531feca6acbae
mjt <58866248@qq.com>
2025-03-23 14:07:05 +0800

    Add MultiplyThreeNums.py

---
+++ MultiplyThreeNums.py
@@ -0,0 +1,9 @@
+def multiply_three_nums(num1, num2, num3):
+return num1 * num2 * num3
+
+if __name__ == "__main__":
+num1 = 2
+num2 = 3
+num3 = 4
+result = multiply_three_nums(num1, num2, num3)
+print(f"The product of the three numbers {num1}, {num2} and {num3} is: {result}")
